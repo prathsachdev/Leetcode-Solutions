74121b7c1784e587236895e81d0b89b667eb1c0e
Pratham Sachdev <prathamsachdev26@gmail.com>
2023-12-06 22:08:22 +0530

    added 0412. Fizz Buzz

--- CPP/0704. Binary Search.cpp
+++ CPP/0704. Binary Search.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    // ITERATIVE
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        
+        while (l <= r) {
+            int mid = (l + r)/2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    // RECURSIVE
     int binarySearch(vector<int>& arr, int l, int r, int searchElement) {
         if (l <= r) {
             int mid = (l + r)/2;
@@ -9,10 +30,10 @@ public:
             }
             
             if (arr[mid] > searchElement) {
-                return binarySearch(arr, l, mid-1, searchElement);
+                return binarySearch(arr, l, mid - 1, searchElement);
             }
             
-            return binarySearch(arr, mid+1, r, searchElement);
+            return binarySearch(arr, mid + 1, r, searchElement);
         } else {
             return -1;
         }
--- Java/0704. Binary Search.java
+++ Java/0704. Binary Search.java
@@ -1,4 +1,25 @@
 class Solution {
+    // ITERATIVE
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        
+        while (l <= r) {
+            int mid = (l + r)/2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    // RECURSIVE
     public int binarySearch(int[] arr, int l, int r, int searchElement) {
         if (l <= r) {
             int mid = (l + r)/2;
@@ -8,10 +29,10 @@ class Solution {
             }
             
             if (arr[mid] > searchElement) {
-                return binarySearch(arr, l, mid-1, searchElement);
+                return binarySearch(arr, l, mid - 1, searchElement);
             }
             
-            return binarySearch(arr, mid+1, r, searchElement);
+            return binarySearch(arr, mid + 1, r, searchElement);
         } else {
             return -1;
         }
